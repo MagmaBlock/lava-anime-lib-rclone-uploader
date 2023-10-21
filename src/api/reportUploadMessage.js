03ceeb47752c93a2ae968b4994c745272024bfb3
岩浆块Magma <magmablock@qq.com>
2023-10-21 20:53:44 +0800

    修复 多重 await

--- src/api/reportUploadMessage.js
+++ src/api/reportUploadMessage.js
@@ -4,10 +4,10 @@ import { LavaAnimeLibAPI } from "../tools/axiosInstance.js";
  * 上报上传成功消息
  * @param {String} index
  * @param {String} fileName
- * @returns
+ * @returns {Promise}
  */
-export async function reportUploadMessage(index, fileName) {
-  return await LavaAnimeLibAPI.post("/v2/report/upload-message", {
+export function reportUploadMessage(index, fileName) {
+  return LavaAnimeLibAPI.post("/v2/report/upload-message", {
     index,
     fileName,
   });
